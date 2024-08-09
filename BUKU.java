db5170350ccee59a877e50aa19c8146535d312e3
DanendraAthallah-uhuy <danendra_indiarto_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 15:24:36 +0700

    Add files via upload

---
+++ BUKU.java
@@ -0,0 +1,15 @@
+public class BUKU {
+
+    int id = 0;
+    String judul = "Kosong";
+    String pengarang = "Kosong";
+    int tahunterbit = 0;
+
+    //Construstor
+    public BUKU(int id, String judul, String pengarang, int tahunterbit){
+        this.id = id;
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.tahunterbit = tahunterbit;
+    }
+}
---
+++ BUKUtes.java
@@ -0,0 +1,45 @@
+public class BUKUtes {
+    public static void main(String[] args) {
+        
+        //Construstor
+        BUKU nendra = new BUKU(99876,"Buku Solusi Kehidupan", "Danendra", 1945);
+        System.out.println(nendra.id);
+        System.out.println(nendra.judul);
+        System.out.println(nendra.pengarang);
+        System.out.println(nendra.tahunterbit);
+        System.out.println("=================");
+
+        BUKU nendr = new BUKU(76871,"Buku Memasak", "Vania", 2015);
+        System.out.println(nendr.id);
+        System.out.println(nendr.judul);
+        System.out.println(nendr.pengarang);
+        System.out.println(nendr.tahunterbit);
+        System.out.println("=================");
+
+        BUKU nend = new BUKU(12345,"Buku Sejarah Proklamasi", "Ir.Soekarno", 1947);
+        System.out.println(nend.id);
+        System.out.println(nend.judul);
+        System.out.println(nend.pengarang);
+        System.out.println(nend.tahunterbit);
+        System.out.println("=================");
+
+        BUKU nen = new BUKU(8976,"Buku Servis Laptop", "Patol", 2010);
+        System.out.println(nen.id);
+        System.out.println(nen.judul);
+        System.out.println(nen.pengarang);
+        System.out.println(nen.tahunterbit);
+        System.out.println("=================");
+
+        BUKU ne = new BUKU(67894,"Buku Gerakan Pramuka", "Jamal Bismillah", 1999);
+        System.out.println(ne.id);
+        System.out.println(ne.judul);
+        System.out.println(ne.pengarang);
+        System.out.println(ne.tahunterbit);
+        System.out.println("=================");
+
+
+
+
+
+    }
+}
